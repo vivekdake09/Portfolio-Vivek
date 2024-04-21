687820f454d99e2f470e30c33baf3f5b471e847d
Ajjack4 <manavjadhav1997@gmail.com>
2024-04-21 18:39:59 +0530

    Added Google Analytics Tag

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 import Home from "./pages/Home";
+import ReactGA from "react-ga"
+const Tracking_id="G-RR30Y16SLS";
 
+ReactGA.initialize(Tracking_id);
 function App() {
+
   return (
       <Home />
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React ,{useEffect} from "react";
 import Hero from "../components/Hero/Hero";
 import Projects from "../components/Projects/Projects";
 import About from "../components/About/About";
@@ -6,8 +6,14 @@ import Contact from "../components/Contact/Contact";
 import Footer from "../components/Footer/Footer";
 import FixSocialIcon from "../components/SocialIcon/FixSocialIcon";
 import ScrollToTop from "../components/SocialIcon/ScrollToTop";
+import ReactGA from "react-ga"
+
+
 
 function Home() {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname);
+  }, []);
   return (
     <>
       <Hero />
